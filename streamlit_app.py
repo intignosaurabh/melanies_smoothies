a030a46578b50e4fd56461ffab67679e4d87f21b
intignosaurabh <127894831+intignosaurabh@users.noreply.github.com>
2024-09-25 21:24:57 +0200

    Update streamlit_app.py
    Updated the variable name "fruit chosen"

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ if ingredients_list:
 
     ingredients_string = ''
 
-    for fruit_choosen in ingredients_list:
-        ingredients_string += fruit_choosen + ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chose)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
